215278d9538c64854d9f73d0e95ef1a4950070cc
tmartinez52 <tmartinez@Timothys-MacBook-Pro.local>
2022-04-14 12:07:31 -0600

    adding db

---
+++ db.tf
@@ -0,0 +1,31 @@
+resource "azurerm_mysql_server" "db" {
+    name = "mysql-tf"
+    location = var.location
+    resource_group_name = var.resource_group_name
+
+
+    sku {
+        name = "B_Gen5_2"
+        capacity = 2
+        tier = "Basic"
+        family = "Gen5"
+    }
+
+    storage_profile {
+        storage_mb = 5120
+        backup_retention_days = 7
+        geo_redundant_backup = "Disabled"
+    }
+    administrator_login = var.username
+    administrator_login_password = var.password
+    version = "5.7"
+    ssl_enforcement = "Enabled"
+}
+
+resource "azurerm_mysql_database" "example" {
+    name = "exampledb"
+    resource_group_name = var.resource_group_name
+    server_name = azurerm_mysql_server.db.name
+    charset = "utf8"
+    collation = "utf8_unicode_ci"
+}
