1b3d3140239380dcab9ea98886608ffec8c730d3
tmartinez52 <tmartinez@Timothys-MacBook-Pro.local>
2022-04-14 10:56:46 -0600

    adding vnet

---
+++ webapp.tf
@@ -0,0 +1,22 @@
+resource "azurerm_app_service_plan" "svcplan" {
+    name = "newweb-appserviceplan"
+    location = var.location
+    resource_group_name = var.resource_group_name
+
+    sku {
+        tier = "Standard"
+        size = "S1"
+    }
+}
+
+resource "azurerm_app_service" "appsvc" {
+    name = "custom-tf-webapp"
+    location = var.location
+    resource_group_name = var.resource_group_name
+    app_service_plan_id = azurerm_app_service_plan.svcplan.id
+
+    site_config {
+      dotnet_framework_version = "v4.0"
+      scm_type = "LocalGit"
+    }
+}
