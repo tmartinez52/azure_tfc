e1eb09e199f5fe0e92ee64e17740aff324f1f770
tmartinez52 <tmartinez@Timothys-MacBook-Pro.local>
2022-04-14 12:13:05 -0600

    adding db

--- db.tf
+++ db.tf
@@ -3,23 +3,14 @@ resource "azurerm_mysql_server" "db" {
     location = var.location
     resource_group_name = var.resource_group_name
 
-
-    sku {
-        name = "B_Gen5_2"
-        capacity = 2
-        tier = "Basic"
-        family = "Gen5"
-    }
-
-    storage_profile {
-        storage_mb = 5120
-        backup_retention_days = 7
-        geo_redundant_backup = "Disabled"
-    }
+    sku_name = "B_Gen5_2"
+    storage_mb = 5120
+    backup_retention_days = 7
     administrator_login = var.username
     administrator_login_password = var.password
     version = "5.7"
-    ssl_enforcement = "Enabled"
+    ssl_enforcement_enabled = true
+    geo_redundant_backup_enabled = false
     depends_on = [
       azurerm_resource_group.rg
     ]
